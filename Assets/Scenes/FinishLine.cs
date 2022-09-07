f47703f1a2a463203a46a7f9adff68fe778305c4
Zorlacto <Marius.15@hotmail.com>
2022-09-07 13:42:24 +0200

    Stuffs

--- Assets/Scenes/FinishLine.cs
+++ Assets/Scenes/FinishLine.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FinishLine : MonoBehaviour
 {
+    public Text timertext;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("player"))
-        {
-            SendMessage("Finnish");
-        }
+        if (collision.CompareTag("Player"))
+           
+            {
+                
+                timertext.color = Color.yellow;
+            }
     }
 
 }
